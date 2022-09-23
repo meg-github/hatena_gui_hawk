8c1a4789a05875bfa1520bdfe9729ab33f29f119
meg-github <fishfishfish@gmail.com>
2022-09-23 17:23:01 +0900

    fix

--- hawk/pages/＊質問回答.py
+++ hawk/pages/＊質問回答.py
@@ -9,5 +9,5 @@ from PIL import Image
 
 
 st.components.v1.html(
-'<iframe width="90%" src="https://docs.google.com/forms/d/e/1FAIpQLScezCAj9ju9SYZKEFzbykPJpWu4WU_QwMfMhyTF-fJ-6YpKQA/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>'
+'<iframe width="95%" src="https://docs.google.com/forms/d/e/1FAIpQLScezCAj9ju9SYZKEFzbykPJpWu4WU_QwMfMhyTF-fJ-6YpKQA/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>'
 ,height = 2000)
