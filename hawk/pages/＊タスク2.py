b89db5418e7597623d3f4192d1908ead63fa03d6
meg-github <fishfishfish@gmail.com>
2022-09-23 10:59:16 +0900

    fix

--- hawk/pages/＊タスク2.py
+++ hawk/pages/＊タスク2.py
@@ -9,10 +9,10 @@ import pickle
 import collections
 
 # # 翌何pyファイル
-sys.path.append('../functions/')
-import scrape_user_comment 
-import analyze_user
-import analyze_text_janome
+# sys.path.append('./functions/')
+import functions.scrape_user_comment as scrape_user_comment
+import functions.analyze_user as analyze_user
+import functions.analyze_text_janome as analyze_text_janome
 
 
 # --------functions---------
